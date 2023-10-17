5757504947135ae67725450b30a91020b35e9144
Mxrcxl03 <Marcel.Koriath@web.de>
2023-10-17 18:24:45 +0200

    Update Neuron.java

--- Neuron.java
+++ Neuron.java
@@ -1,23 +1,8 @@
 public abstract class Neuron extends NeuronalesNetz{
     protected double value;
-    private double weight[];
     public Neuron(){
         this.value = 0;
     }
-    public double compute(double[] input){
-        double result = 0;
-        for(int i = 0; i < input.length; i++) {
-            result+= input[i] * weight[i];
-        }
-        return sigmoid(result);
-    }
-    private double sigmoid(double x) {
-        return 1 / (1 + Math.exp(-x));
-    }
-    private double tanh(double x) {
-        return (Math.exp(x)-Math.exp(-x)/Math.exp(x)+Math.exp(-x));
-    }
-
     public abstract double getValue();
     public abstract int getNID();
 }
