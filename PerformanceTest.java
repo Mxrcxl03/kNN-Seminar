29a2868fce130ed5856b8fac90d471e2c21fd991
Mxrcxl03 <Marcel.Koriath@web.de>
2023-10-29 00:15:40 +0200

    PerformanceTest2++

--- PerformanceTest.java
+++ PerformanceTest.java
@@ -40,28 +40,27 @@ public class PerformanceTest {
         String id = "id";
         String logi = "logi";
         String tanh = "tanh";
-        int[] netzkonf1 = {3, 3, 4};
-        double[] input1 = {1, 0, 0};
+        int[] netzkonf1 = {2, 2};
+        double[] input1 = {1, 0};
         int[] netzkonf2 = {100, 100, 101};
         double[] input2 = new double[100];
-        Random random = new Random();
-        for(int i = 0; i < input2.length; i++){
-            input2[i] = random.nextGaussian(-1,1);
-        }
         double[][][] gewichtsdatei1 = {
                 { {-0.081, 0.08, -0.04}, {0.06, 0.02, -0.003}, {-0.01, 0.003, -0.09}, {0.08, -0.09, -0.05} },
                 { {-0.008, 0.01, 0.01, 2.9E-4}, {0.06, -0.06, -0.027, -0.01}, {0.04, 0.06, 0.08, 0.08}, {-0.08, 0.06, 0.09, -0.001} }};
         NeuronalesNetz n1 = new NeuronalesNetz();
+        double[][][] gewichtsdatei2 = {
+                {{0.01, 0,02}, {0.3,-0.9}}
+        };
 
         Duration cpuTime = measureCpuTime(() -> {
-            n1.initNetz(netzkonf2);
+            n1.initNetz(netzkonf1);
             n1.initGewichteRandom();
 
-            for(int i = 0; i < netzkonf1[1]; i++) {
+            /*for(int i = 0; i < netzkonf1[1]; i++) {
                 n1.layer[1][i].setActivationFunction(logi);
-            }
+            }*/
 
-            n1.computeNN(input2);
+            n1.computeNN(input1);
         });
 
         System.out.println("CPU Time: " + cpuTime.toMillis() + " milliseconds");
